5cf8b3753a6bb43e2ea37ed26013b08871479894
Karen Buckey <jamesabuckey@gmail.com>
2019-05-13 16:39:31 -0400

    Feat: move data from view into controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,7 +4,8 @@ module.exports.homelist = function(req, res){
 		title: 'Loc8r - find a place to work with wifi',
 		pageHeader: {
 			title: 'Loc8r',
-			strapline: 'Find places to work with wifi near you!'	
+			strapline: 'Find places to work with wifi near you!',
+      sidebar: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint\? Let Loc8r help you find the place you\'re looking for.',	
 		},
     locations: [{
       name: 'Starcups',
@@ -24,14 +25,59 @@ module.exports.homelist = function(req, res){
       rating: 2,
       facilities: ['Food', 'Premium wifi'],
       distance: '250m'
-    }] 
+    }]
 	});
 };
 
 /* Get 'Location info' page */
-module.exports.locationInfo = function(req, res){
-	res.render('location-info', { title: 'Location info' });
+module.exports.locationInfo = function(req, res) {
+	res.render('location-info', { 
+    title: 'Location info',
+    pageHeader: {
+      title: 'Starcups'
+    },
+    sidebar: {
+      contex: 'is on Loc84 because it has accessible wifi and space to sit down with your laptop and get some work done.',
+      callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+    },
+    location: {
+      name: 'Starcups',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      cords: {
+        lat: 51.455041,
+        lng: -0.9690884
+      },
+      openingTimes: [{
+        days: 'Monday - Friday',
+        opening: '7:00am',
+        closing: '7:00pm',
+        closed: false
+      }, {
+        days: 'Saturday',
+        opening: '8:00am',
+        closing: '5:00pm',
+        closed: false
+      }, {
+        days: 'Sunday',
+        closed: true
+      }],
+      reviews: [{
+        author: 'Simon Holmes',
+        rating: 5,
+        timestamp: '16 July 2013',
+        reviewText: 'What a great place. I can\'t say enough good things about it.'
+      }, {
+        name: 'Charlie Chaplin',
+        rating: 3,
+        timestamp: '3 July 2013',
+        reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+      }]
+    }
+  });
 };
+
 
 /* Get 'Add review' page */
 module.exports.addReview = function(req, res){
