113c43d4d4d3fda5978edc0041682270e61b0e47
James Buckey <jamesabuckey@gmail.com>
2019-06-20 12:27:52 -0400

    feat: add api get method for spesific reviews

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -9,14 +9,50 @@ var sendJsonREsponse = function (res, status, content) {
 
 module.exports.reviewsCreate = function(req, res) { };
 module.exports.reviewsReadOne = function(req, res) {
-  Loc
+  if(req.params && req.params.locationid && req.params.reviewid){
+    Loc
     .findById(req.params.locationid)
     .select('name reviews')
     .exec(
       function(err, location) {
-        var review;
-        review = location.reviews.id(req.params.reviewid);
-      })
+        var response, review;
+        if (!location) {
+          sendJsonREsponse(res, 404, {
+            "message": "location not found"
+          });
+          return;
+        } else if (err) {
+          sendJsonREsponse(res, 400, err);
+          return;
+        }
+        if (location.reviews && location.reviews.length > 0) {
+          review = location.reviews.id(req.params.reviewid);
+          if (!review) {
+            sendJsonREsponse(res, 404, {
+              "message": "review not found"
+            });
+          } else {
+            response = {
+              location : {
+                name : location.name,
+                id : req.params.locationid
+              },
+              review : review
+            };
+            sendJsonREsponse(res, 200, response);
+          }  
+        } else {
+          sendJsonREsponse(res, 404, {
+            "message": "No reviews found"
+          });
+        }
+      }
+    );
+  } else {
+    sendJsonREsponse (res, 404, {
+      "message": "Not found, locationid and review are both required"
+    });
+  }
 };
 module.exports.reviewsUpdateOne = function(req, res) { };
 module.exports.reviewsDeleteOne = function(req, res) { };
