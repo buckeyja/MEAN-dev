e870e5f4ec86e850caf8560dcacd1177680458ef
James Buckey <jamesabuckey@gmail.com>
2019-07-06 09:47:30 -0400

    fix: Make routes more clear by updating formatting

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,16 +4,26 @@ var ctrlLocations = require('../controllers/locations');
 var ctrlReviews =require('../controllers/reviews');
 
 // Locations
-// router.get('/locations', ctrlLocations.locationsListByDistance); Unusable because geoNear is depricated in Mongoose.
-router.post('/locations', ctrlLocations.locationsCreate);
-router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
-router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
-router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
+router
+  .route('/locations')
+  .get(ctrlLocations.locationsListByDistance)
+  .post(ctrlLocations.locationsCreate);
+
+router
+  .route('/locaitons/:locationid')
+  .get(ctrlLocations.locationsReadOne)
+  .put(ctrlLocations.locationsUpdateOne)
+  .delete(ctrlLocations.locationsDeleteOne);
 
 // Reviews
-router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
-router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
-router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
-router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
+router
+  .route('/locations/:locationid/reviews')
+  .post(ctrlReviews.reviewsCreate);
 
+router
+  .route('/locations/:locationid/reviews/:reviewid')
+  .get(ctrlReviews.reviewsReadOne)
+  .put(ctrlReviews.reviewsUpdateOne)
+  .delete(ctrlReviews.reviewsDeleteOne);
+  
 module.exports = router;
