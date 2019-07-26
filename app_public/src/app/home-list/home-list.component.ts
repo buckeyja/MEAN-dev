58f181aba046e91192acc6f5bc9805963244ae0b
James Buckey <jamesabuckey@gmail.com>
2019-07-26 08:01:57 -0400

    feat: pull data from db into home-list-component

--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Loc8rDataService } from '../loc8r-data.service';
 
 export class Location {
   _id: string;
@@ -12,29 +13,40 @@ export class Location {
 @Component({
   selector: 'app-home-list',
   templateUrl: './home-list.component.html',
-  styleUrls: ['./home-list.component.css']
+  styleUrls: ['./home-list.component.css'],
+  providers: [Loc8rDataService]
 })
 export class HomeListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loc8rDataService: Loc8rDataService) { }
 
-  locations: Location[] = [{
-    _id: '590d8dc7a7cb5b8e3f1bfc48',
-    name: 'Costy',
-    distance: 14000.1234,
-    address: 'High Street, Reading',
-    rating: 3,
-    facilities: ['hot drinks', 'food', 'power']
-  }, {
-    _id: '590d8dc7a7cb5b8e3f1bfc48',
-    name: 'Starcups',
-    distance: 120.542,
-    address: 'High Street, Reading',
-    rating: 5,
-    facilities: ['wifi', 'food', 'hot drinks']
-  }];
+  public locations: Location[];
+  // locations: Location[] = [{
+  //   _id: '590d8dc7a7cb5b8e3f1bfc48',
+  //   name: 'Costy',
+  //   distance: 14000.1234,
+  //   address: 'High Street, Reading',
+  //   rating: 3,
+  //   facilities: ['hot drinks', 'food', 'power']
+  // }, {
+  //   _id: '590d8dc7a7cb5b8e3f1bfc48',
+  //   name: 'Starcups',
+  //   distance: 120.542,
+  //   address: 'High Street, Reading',
+  //   rating: 5,
+  //   facilities: ['wifi', 'food', 'hot drinks']
+  // }];
+
+  private getLocations(): void {
+    this.loc8rDataService
+      .getLocations()
+        .then(foundLocations => {
+          this.locations = foundLocations;
+        });
+  }
 
   ngOnInit() {
+    this.getLocations(); // ensures that the loc8rDataService is avaible when it is called by HomeListComponent
   }
 
 }
