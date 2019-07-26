4d0d9e60377bba54109e6b2a59ccce989f4e53a9
James Buckey <jamesabuckey@gmail.com>
2019-07-26 08:28:49 -0400

    fix: import AppComponent into app.module to fix compilation error

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppComponent } from './app.component';
 import { HomeListComponent } from './home-list/home-list.component';
 import { DistancePipe } from './distance.pipe';
 
