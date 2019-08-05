37c110c28cb0d4ecd945bfb5c4d5fe87a5b4527b
James Buckey <jamesabuckey@gmail.com>
2019-08-05 10:19:15 -0400

    feat: Add review functionality to location details page

--- app_public/src/app/location-details/location-details.component.ts
+++ app_public/src/app/location-details/location-details.component.ts
@@ -10,7 +10,15 @@ export class LocationDetailsComponent implements OnInit {
 
   @Input() location: Location;
 
-  public googleAPIKey: string = '<AIzaSyAJsF4qbHF9mQenMsDpIlYbL5ThEKxwCR4>'
+  public googleAPIKey: string = '<AIzaSyAJsF4qbHF9mQenMsDpIlYbL5ThEKxwCR4>';
+
+  public formVisible: boolean = false;
+  
+  public newReview = {
+    author: '',
+    rating: 5,
+    reviewText: ''
+  };
 
   constructor() { }
 
