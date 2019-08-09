dfdcaade0b08ba11e01412fb672a1e1af52f5ac0
James Buckey <jamesabuckey@gmail.com>
2019-08-09 13:02:08 -0400

    feat: add functionality to determine if users are logged in and current in angular

--- app_public/src/app/authentication.service.ts
+++ app_public/src/app/authentication.service.ts
@@ -27,6 +27,24 @@ export class AuthenticationService {
       .then((authResp: AuthResponse) => this.saveToken(authResp.token));
   }
 
+  public isLoggedIn(): boolean {
+    const token: string = this.getToken();
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      return payload.exp > (Date.now() / 1000);
+    } else {
+      return false;
+    }
+  }
+
+  public getCurrentUser(): User {
+    if (this.isLoggedIn()) {
+      const token: string = this.getToken();
+      const { email, name } = JSON.parse(atob(token.split('.')[1]));
+      return { email, name } as User;
+    }
+  }
+
   public register(user: User): Promise<any> {
     return this.loc8rDataService.register(user)
       .then((authResp: AuthResponse) => this.saveToken(authResp.token));
