6979cc4a3ecf5939307e9015a389a8c6d389f56c
James Buckey <jamesabuckey@gmail.com>
2019-08-14 10:59:20 -0400

    feat: manage user authorization when adding reviews

--- app_public/src/app/loc8r-data.service.ts
+++ app_public/src/app/loc8r-data.service.ts
@@ -1,22 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Location, Review } from './location';
 import { User } from './user';
 import { AuthResponse } from './authresponse';
+import { BROWSER_STORAGE } from './storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Loc8rDataService {
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    @Inject(BROWSER_STORAGE) private storage: Storage) {
 
   }
 
-  private apiBaseUrl = 'http://localhost:3000/api/';
+  private apiBaseUrl = 'http://localhost:3000/api/'; // There is an extra '/' here but it keeps there from beeing a CHORS error
 
   private makeAuthApiCall(urlPath: string, user: User): Promise<AuthResponse> {
-    const url: string = `${this.apiBaseUrl}/${urlPath}`;
+    const url: string = `${this.apiBaseUrl}${urlPath}`;
     return this.http
       .post(url, user)
       .toPromise()
@@ -45,9 +48,14 @@ export class Loc8rDataService {
   }
 
   public addReviewByLocationId(locationId: string, formData: Review): Promise<Review> {
-    const url: string = `${this.apiBaseUrl}/locations/${locationId}/reviews`;
+    const url: string = `${this.apiBaseUrl}locations/${locationId}/reviews`;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': `Bearer ${this.storage.getItem('loc8r-token')}`
+      })
+    }
     return this.http
-      .post(url, formData)
+      .post(url, formData, httpOptions)
       .toPromise()
       .then(response => response as Review)
       .catch(this.handleError);
